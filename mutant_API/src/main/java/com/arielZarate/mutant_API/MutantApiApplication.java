474200e3274df4f6ad2b8171cfa6bdeced59b952
arielZarate <arieltecnico@gmail.com>
2024-11-12 18:25:54 -0300

    iniciando creacion de proyecto api mutante

---
+++ mutant_API/src/main/java/com/arielZarate/mutant_API/MutantApiApplication.java
@@ -0,0 +1,13 @@
+package com.arielZarate.mutant_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantApiApplication.class, args);
+	}
+
+}
